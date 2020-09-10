5191e5d2781aac971ae688394056f601aafce5ae
INOCcent-afk <michael828inoc@gmail.com>
2020-09-10 12:41:15 +0800

    cube-slider

---
+++ main.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".swiper-container", {
+  effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: false,
+    slideShow: false,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
